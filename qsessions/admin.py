eb9ded3be26f400060b96b77179e38611c862425
Yazdan Ranjbar <yazdan_ra@icloud.com>
2020-08-25 14:07:40 +0430

    - Apply black

--- qsessions/admin.py
+++ qsessions/admin.py
@@ -19,10 +19,10 @@ def linkify(field_name):
     def _linkify(obj):
         linked_obj = getattr(obj, field_name)
         if linked_obj is None:
-            return '-'
+            return "-"
         app_label = linked_obj._meta.app_label
         model_name = linked_obj._meta.model_name
-        view_name = f'admin:{app_label}_{model_name}_change'
+        view_name = f"admin:{app_label}_{model_name}_change"
         link_url = reverse(view_name, args=[linked_obj.pk])
         return format_html('<a href="{}">{}</a>', link_url, linked_obj)
 
