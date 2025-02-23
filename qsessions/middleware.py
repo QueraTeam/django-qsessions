41b3a89aa0f6638442dc74ee0751a8ab948e0830
Mohammad Javad Naderi <mjnaderi@gmail.com>
2025-02-23 12:14:59 +0330

    Get client address from REMOTE_ADDR

--- qsessions/middleware.py
+++ qsessions/middleware.py
@@ -1,13 +1,12 @@
 from django.conf import settings
 from django.contrib.sessions.middleware import SessionMiddleware as DjSessionMiddleware
-from ipware import get_client_ip
 
 
 class SessionMiddleware(DjSessionMiddleware):
     def process_request(self, request):
         session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME)
         request.session = self.SessionStore(
-            ip=get_client_ip(request)[0],
+            ip=request.META.get("REMOTE_ADDR", None),
             user_agent=request.headers.get("User-Agent", ""),
             session_key=session_key,
         )
