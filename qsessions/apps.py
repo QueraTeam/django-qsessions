521222630ac920c63b8703fc160369c3eeb17c31
Mohammad Javad Naderi <mjnaderi@gmail.com>
2025-02-23 13:42:56 +0330

    Change app verbose_name to "Sessions"

--- qsessions/apps.py
+++ qsessions/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class DjangoQsessionsConfig(AppConfig):
     name = "qsessions"
+    verbose_name = _("Sessions")
